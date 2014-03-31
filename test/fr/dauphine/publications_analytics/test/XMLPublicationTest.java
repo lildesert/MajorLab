f919506dc1cd76255ccba07aecadfe16eeee3f29
extinguisher <lynxsilver@hotmail.fr>
2014-03-31 14:39:20 +0200

    import

--- test/fr/dauphine/publications_analytics/test/XMLPublicationTest.java
+++ test/fr/dauphine/publications_analytics/test/XMLPublicationTest.java
@@ -2,6 +2,8 @@ package fr.dauphine.publications_analytics.test;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.Test;
 
 import fr.dauphine.publications_analytics.src.XMLPublication;
