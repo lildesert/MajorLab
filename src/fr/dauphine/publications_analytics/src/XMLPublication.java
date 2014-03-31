8f282ae83c826229e9a4222f5725ece8ed8ae698
lildesert <geonosis8@hotmail.com>
2014-03-31 14:16:20 +0200

    Update XMLPublication.java

--- src/fr/dauphine/publications_analytics/src/XMLPublication.java
+++ src/fr/dauphine/publications_analytics/src/XMLPublication.java
@@ -276,4 +276,272 @@ public class XMLPublication {
 		return number_of_bookchapters_per_author;
 		
 	}
+	
+	
+	
+	//David
+	public int getNumberOfConferenceProcessing(String fileName) {
+		
+		int nbConf = 0;
+
+		String xml_file = getClass().getResource(fileName).toExternalForm();
+
+		String query = "for $x in doc(\"" + xml_file + "\")/dblp "
+				+ "return count(for $y in $x/inproceedings return 1)";
+
+		System.out.println("XQuery query:" + query);
+
+		try {
+			XQDataSource ds = new SaxonXQDataSource();
+			XQConnection conn = ds.getConnection();
+			XQExpression exp = conn.createExpression();
+
+			XQSequence seq = exp.executeQuery(query);
+
+			seq.next();
+
+			nbConf = seq.getInt();
+
+			System.out.println("Number of inprocessing of is "
+					+ nbConf);
+
+			seq.close();
+
+		} catch (XQException err) {
+			System.out.println("Failed as expected: " + err.getMessage());
+		}
+
+		return nbConf;
+	}
+	
+	public int get_number_of_publication_for_one_author(String file_name, String author) {
+
+		int number_of_publications = 0;
+
+		String xml_file = getClass().getResource(file_name).toExternalForm();
+
+		String query = "for $x in doc(\"" + xml_file + "\")/dblp "
+				+ "return count(for $y in $x/* where $y/author='"+ author +"' return 1)";
+
+
+		System.out.println("XQuery query:" + query);
+
+		try {
+			XQDataSource ds = new SaxonXQDataSource();
+			XQConnection conn = ds.getConnection();
+			XQExpression exp = conn.createExpression();
+
+			XQSequence seq = exp.executeQuery(query);
+
+			seq.next();
+
+			number_of_publications = seq.getInt();
+
+			System.out.println("Number of publications of is "
+					+ number_of_publications);
+
+			seq.close();
+
+		} catch (XQException err) {
+			System.out.println("Failed as expected: " + err.getMessage());
+		}
+
+		return number_of_publications;
+
+
+	}
+	
+	public List<String> getAuthor(String file_name) {
+
+		List<String> authors = new ArrayList<String>();
+
+		String xml_file = getClass().getResource(file_name).toExternalForm();
+
+		String query = "for $x in doc(\"" + xml_file + "\")/dblp "
+				+ "return distinct-values($x/*/author/text())";
+
+
+		System.out.println("XQuery query:" + query);
+
+		try {
+			XQDataSource ds = new SaxonXQDataSource();
+			XQConnection conn = ds.getConnection();
+			XQExpression exp = conn.createExpression();
+
+			XQSequence seq = exp.executeQuery(query);
+
+			while(seq.next())
+			{
+				authors.add(seq.getItemAsString(null));
+			}	
+			
+			System.out.println("Liste auteurs " + authors);
+			seq.close();
+
+		} catch (XQException err) {
+			System.out.println("Failed as expected: " + err.getMessage());
+		}
+
+		Collections.sort(authors);
+		return authors;
+
+	}
+	
+	
+	
+	//Vincent
+	public double get_mean_number_of_authors_per_publicationC(String file_name) {
+		
+		double mean = 0.0;
+		
+		double num_publications = this.get_number_of_publicationsC(file_name);
+		double num_authors = this.get_number_of_author_appearancesC(file_name);
+		
+		mean = num_authors/num_publications;
+		
+		return mean;
+		
+	}
+	
+public int get_number_of_books_appearancesC(String file_name) {
+		
+		int number_of_author_appearances = 0;
+		
+		String xml_file = getClass().getResource(file_name).toExternalForm();
+		
+		String query = "for $x in doc(\"" +xml_file+ "\")/dblp " +
+				"return count(for $y in $x/book return 1)";
+		
+		System.out.println("XQuery query:"+query);
+
+		try{
+			XQDataSource ds = new SaxonXQDataSource();
+			XQConnection conn = ds.getConnection();
+			XQExpression exp = conn.createExpression();
+	
+	
+			XQSequence seq = exp.executeQuery(query);			
+	
+			seq.next();
+			
+			number_of_author_appearances = seq.getInt();
+	
+			System.out.println("Number of books of is "+number_of_author_appearances);
+			
+			seq.close();
+	
+		} catch (XQException err) {
+			System.out.println("Failed as expected: " + err.getMessage());
+		}
+		
+		return number_of_author_appearances;
+		
+	}
+
+
+public int get_number_of_chapters_appearancesC(String file_name) {
+	
+	int number_of_author_appearances = 0;
+	
+	String xml_file = getClass().getResource(file_name).toExternalForm();
+	
+	String query = "for $x in doc(\"" +xml_file+ "\")/dblp " +
+			"return count(for $y in $x/incollection return 1)";
+	
+	System.out.println("XQuery query:"+query);
+
+	try{
+		XQDataSource ds = new SaxonXQDataSource();
+		XQConnection conn = ds.getConnection();
+		XQExpression exp = conn.createExpression();
+
+
+		XQSequence seq = exp.executeQuery(query);			
+
+		seq.next();
+		
+		number_of_author_appearances = seq.getInt();
+
+		System.out.println("Number of chapters of is "+number_of_author_appearances);
+		
+		seq.close();
+
+	} catch (XQException err) {
+		System.out.println("Failed as expected: " + err.getMessage());
+	}
+	
+	return number_of_author_appearances;
+	
+}
+
+public int get_number_of_conferencesProcedings_per_authorsC(String file_name,String author) {
+	
+	int number_of_author_appearances = 0;
+	
+	String xml_file = getClass().getResource(file_name).toExternalForm();
+	
+	String query = "for $x in doc(\"" +xml_file+ "\")/dblp " +
+				" return count(for $y in $x/inproceedings where $y/author= \"" + author +
+			"\" return 1)";
+	
+	System.out.println("XQuery query:"+query);
+
+	try{
+		XQDataSource ds = new SaxonXQDataSource();
+		XQConnection conn = ds.getConnection();
+		XQExpression exp = conn.createExpression();
+
+
+		XQSequence seq = exp.executeQuery(query);			
+
+		seq.next();
+		
+		number_of_author_appearances = seq.getInt();
+
+		System.out.println("Number of conference proceedings for the author " + author + " is "+number_of_author_appearances);
+		
+		seq.close();
+
+	} catch (XQException err) {
+		System.out.println("Failed as expected: " + err.getMessage());
+	}
+	
+	return number_of_author_appearances;
+	
+}
+	
+
+public List<String> get_list_of_authors_appearancesC(String file_name) {
+		
+	String xml_file = getClass().getResource(file_name).toExternalForm();
+	
+	String query = "for $x in doc(\"" +xml_file+ "\")/dblp " +
+			"return distinct-values($x/*/author/text())";
+	
+	System.out.println("XQuery query:"+query);
+	
+	List<String> listAuthor = new ArrayList();
+
+	try{
+		XQDataSource ds = new SaxonXQDataSource();
+		XQConnection conn = ds.getConnection();
+		XQExpression exp = conn.createExpression();
+
+
+		XQSequence seq = exp.executeQuery(query);			
+
+		while(seq.next())	{
+			listAuthor.add(seq.getItemAsString(null));
+		}
+		
+		System.out.println("Number of authors of is "+ listAuthor.size());
+		
+		seq.close();
+
+	} catch (XQException err) {
+		System.out.println("Failed as expected: " + err.getMessage());
+	}
+	
+	return listAuthor;
+}
 }
