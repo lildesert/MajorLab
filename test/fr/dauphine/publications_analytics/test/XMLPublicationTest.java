8e5d8e13f3cca7f33b7eb5337e9f9e094a9639b5
lildesert <geonosis8@hotmail.com>
2014-03-31 14:19:43 +0200

    Update XMLPublicationTest.java

--- test/fr/dauphine/publications_analytics/test/XMLPublicationTest.java
+++ test/fr/dauphine/publications_analytics/test/XMLPublicationTest.java
@@ -82,6 +82,83 @@ public class XMLPublicationTest {
 		XMLPublication xb = new XMLPublication();
 		assertEquals(1, xb.get_number_of_bookchapters_per_author(file_name, "Lei Dang"));
 	}
+	
+	
+	//Vincent
+	@Test
+	public void should_support_multiple_books() throws Exception	{
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		assertEquals(8,
+				xb.get_number_of_books_appearancesC(file_name));
+
+	}
+	
+	@Test
+	public void should_support_multiple_chapters() throws Exception	{
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		assertEquals(39,
+				xb.get_number_of_chapters_appearancesC(file_name));
+
+	}
+	
+	@Test
+	public void should_support_multiple_conferencesProcedings_per_authorsC() throws Exception	{
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		List<String> listAuthor = xb.get_list_of_authors_appearancesC(file_name);
+		
+		for(int i = 0;i<listAuthor.size();i++)	{
+			System.out.println(listAuthor.get(i));
+		}
+	}
+	
+	@Test
+	public void Test_per_authorsC() throws Exception	{
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		List<String> listAuthor = xb.get_list_of_authors_appearancesC(file_name);
+		
+		HashMap<String,Integer> authorPublicationMap = new HashMap();
+		
+		for(int i = 0;i<listAuthor.size();i++)	{
+			authorPublicationMap.put(listAuthor.get(i), xb.get_number_of_conferencesProcedings_per_authorsC(file_name,listAuthor.get(i)));
+		}
+		
+		for(int j = 0;j<listAuthor.size();j++)
+			System.out.println("Auteur = " + listAuthor.get(j) + " Number of conf="+ authorPublicationMap.get(listAuthor.get(j)));
+	}
+	
+	
+	
+	//David
+	@Test
+	public void conferenceProceedingNumberTest() throws Exception {
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		assertEquals(527,
+				xb.getNumberOfConferenceProcessing(file_name));
+		
+	}
+	
+	@Test
+	public void number_of_publication_for_one_authorTest() throws Exception {
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		assertEquals(1,
+				xb.get_number_of_publication_for_one_author(file_name, "Dale Vincent"));
+		
+	}
+	
+	@Test
+	public void getAuthorTest() throws Exception {
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		assertEquals(1139,
+				xb.getAuthor(file_name).size());
+		
+	}
 
 }
 
