1167b77b84fe0247a20244adc148552fce780a78
TionParti <TionParti@tionpartis-air>
2014-04-07 14:06:53 +0200

    MAJ 2 full

--- src/fr/dauphine/publications_analytics/src/XMLPublication.java
+++ src/fr/dauphine/publications_analytics/src/XMLPublication.java
@@ -875,4 +875,74 @@ public int get_number_of_articles_per_year(String file_name,String year,String a
 	return number_of_year_appearances;
 	
 }
+
+//Question 4.D
+public int get_number_of_books_per_year(String file_name,String year,String author) {
+	
+	int number_of_year_appearances = 0;
+	
+	String xml_file = getClass().getResource(file_name).toExternalForm();
+
+	String query = "for $x in doc(\"" +xml_file+ "\")/dblp " +
+				" return count(for $y in $x/book where $y/year= \"" + year +
+			"\" and $y/author = \"" + author + "\" return 1)";
+
+	try{
+		XQDataSource ds = new SaxonXQDataSource();
+		XQConnection conn = ds.getConnection();
+		XQExpression exp = conn.createExpression();
+
+
+		XQSequence seq = exp.executeQuery(query);			
+
+		seq.next();
+		
+		number_of_year_appearances = seq.getInt();
+
+		//System.out.println("Number of publications for the year " + year + " is "+number_of_year_appearances);
+		
+		seq.close();
+
+	} catch (XQException err) {
+		System.out.println("Failed as expected: " + err.getMessage());
+	}
+	
+	return number_of_year_appearances;
+	
+}
+
+//Question 4.D
+public int get_number_of_booksChapter_per_year(String file_name,String year,String author) {
+	
+	int number_of_year_appearances = 0;
+	
+	String xml_file = getClass().getResource(file_name).toExternalForm();
+
+	String query = "for $x in doc(\"" +xml_file+ "\")/dblp " +
+				" return count(for $y in $x/incollection where $y/year= \"" + year +
+			"\" and $y/author = \"" + author + "\" return 1)";
+
+	try{
+		XQDataSource ds = new SaxonXQDataSource();
+		XQConnection conn = ds.getConnection();
+		XQExpression exp = conn.createExpression();
+
+
+		XQSequence seq = exp.executeQuery(query);			
+
+		seq.next();
+		
+		number_of_year_appearances = seq.getInt();
+
+		//System.out.println("Number of publications for the year " + year + " is "+number_of_year_appearances);
+		
+		seq.close();
+
+	} catch (XQException err) {
+		System.out.println("Failed as expected: " + err.getMessage());
+	}
+	
+	return number_of_year_appearances;
+	
+}
 }
--- test/fr/dauphine/publications_analytics/test/XMLPublicationTest.java
+++ test/fr/dauphine/publications_analytics/test/XMLPublicationTest.java
@@ -272,7 +272,7 @@ public class XMLPublicationTest {
 			System.out.println("Année = " + listYear.get(j) + " Number of author who wrote a conference proceedings this year ="+ yearPublicationMap.get(listYear.get(j)));
 	}
 	
-	@Test
+	//@Test
 	public void Test_number_of_author_articles_per_year() throws Exception	{
 		String file_name = "dblp_curated_sample.xml";
 		XMLPublication xb = new XMLPublication();
@@ -299,6 +299,63 @@ public class XMLPublicationTest {
 		for(int j = 0;j<listYear.size();j++)
 			System.out.println("Année = " + listYear.get(j) + " Number of author who wrote a journal article this year ="+ yearPublicationMap.get(listYear.get(j)));
 	}
+	
+	//@Test
+		public void Test_number_of_author_books_per_year() throws Exception	{
+			String file_name = "dblp_curated_sample.xml";
+			XMLPublication xb = new XMLPublication();
+			List<String> listYear = xb.get_list_of_years_appearancesC(file_name);
+			List<String> listAuthor = xb.get_list_of_authors_appearancesC(file_name);
+			int cptAuthor = 0,nbPublication=0;
+			
+			//year:number of author
+			HashMap<String,Integer> yearPublicationMap = new HashMap();
+
+			for(int i = 0;i<listYear.size();i++)	{
+				for(int j = 0;j<listAuthor.size();j++)	{
+
+					//s'il a ecrit qlq chose cette année
+					nbPublication = xb.get_number_of_books_per_year(file_name,listYear.get(i),listAuthor.get(j));
+
+					if(nbPublication>0)
+						cptAuthor ++;
+				}	
+				nbPublication=0;
+				yearPublicationMap.put(listYear.get(i),cptAuthor);
+				cptAuthor = 0;
+			}
+			for(int j = 0;j<listYear.size();j++)
+				System.out.println("Année = " + listYear.get(j) + " Number of author who wrote a book this year ="+ yearPublicationMap.get(listYear.get(j)));
+		}
+	
+	@Test
+	public void Test_number_of_bookChapter_books_per_year() throws Exception	{
+		String file_name = "dblp_curated_sample.xml";
+		XMLPublication xb = new XMLPublication();
+		List<String> listYear = xb.get_list_of_years_appearancesC(file_name);
+		List<String> listAuthor = xb.get_list_of_authors_appearancesC(file_name);
+		int cptAuthor = 0,nbPublication=0;
+		
+		//year:number of author
+		HashMap<String,Integer> yearPublicationMap = new HashMap();
+
+		for(int i = 0;i<listYear.size();i++)	{
+			for(int j = 0;j<listAuthor.size();j++)	{
+
+				//s'il a ecrit qlq chose cette année
+				nbPublication = xb.get_number_of_booksChapter_per_year(file_name,listYear.get(i),listAuthor.get(j));
+
+				if(nbPublication>0)
+					cptAuthor ++;
+			}	
+			nbPublication=0;
+			yearPublicationMap.put(listYear.get(i),cptAuthor);
+			cptAuthor = 0;
+		}
+		for(int j = 0;j<listYear.size();j++)
+			System.out.println("Année = " + listYear.get(j) + " Number of author who wrote a book chapter this year ="+ yearPublicationMap.get(listYear.get(j)));
+	}
+		
 	
 	//@Test
 	public void Test_number_of_chapter_per_year() throws Exception	{
