f3c9196d4ce149f25ce536b809a18c9b103ee0de
extinguisher <lynxsilver@hotmail.fr>
2014-04-28 14:19:25 +0200

    Nouveau package + classe de caluclus

---
+++ src/fr/dauphine/publications_analytics_task3/src/Calcul.java
@@ -0,0 +1,40 @@
+package fr.dauphine.publications_analytics_task3.src;
+
+public class Calcul {
+	
+	public static double mean(double[] m) {
+	    double sum = 0;
+	    for (int i = 0; i < m.length; i++) {
+	        sum += m[i];
+	    }
+	    return sum / m.length;
+	}
+	
+	public static double median(double[] m) {
+	    int middle = m.length/2;
+	    if (m.length%2 == 1) {
+	        return m[middle];
+	    } else {
+	        return (m[middle-1] + m[middle]) / 2.0;
+	    }
+	}
+	
+	public static int mode(int a[]) {
+	    int maxValue = 0;
+	    int maxCount = 0;
+
+	    for (int i = 0; i < a.length; ++i) {
+	        int count = 0;
+	        for (int j = 0; j < a.length; ++j) {
+	            if (a[j] == a[i]) ++count;
+	        }
+	        if (count > maxCount) {
+	            maxCount = count;
+	            maxValue = a[i];
+	        }
+	    }
+
+	    return maxValue;
+	}
+	
+}
