19d11cb8df328ef252446e5e1c9edcd38b4286f7
extinguisher <lynxsilver@hotmail.fr>
2014-04-28 16:18:39 +0200

    commit

--- src/fr/dauphine/publications_analytics_task3/src/Author.java
+++ src/fr/dauphine/publications_analytics_task3/src/Author.java
@@ -8,7 +8,7 @@ import fr.dauphine.publications_analytics.src.UserStory2;
 
 public class Author {
 	
-	public void getAverageNumberOfPublications(UserStory2 us2) throws Exception {
+	public HashMap<String, Double> getAverageNumberOfPublications(UserStory2 us2) throws Exception {
 		String file_name = "dblp_curated_sample.xml";
 		List<String> listAuthor = us2.getAuthor(file_name);
 		List <Integer> results = new LinkedList();
@@ -16,15 +16,7 @@ public class Author {
 		for (int i = 0; i < listAuthor.size(); i++) {
 			results.add(us2.getNumberOfPublicationForOneAuthor(file_name, listAuthor.get(i)));
 		}
-
-		double mean = Calcul.mean(results);
-		
-		System.out.println("Mean = " + mean);
-		
-		double median = Calcul.median(results);
-		System.out.println("Median = " + median);
-		int mode = Calcul.mode(results);
-		System.out.println("Mode = " + mode);
+		return Calcul.computeAll(results);
 	}
 	
 	public void getAverageNumberJournalArticles(UserStory2 us2) throws Exception {
