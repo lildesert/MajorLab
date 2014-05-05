f2a7a5bde4d13572c0b6e49db9cf9c1ae6057ba2
TionParti <TionParti@172.20.10.3>
2014-05-05 15:51:26 +0200

    commit number per year Q3

--- src/fr/dauphine/publications_analytics_task3/src/Year.java
+++ src/fr/dauphine/publications_analytics_task3/src/Year.java
@@ -139,4 +139,111 @@ public class Year {
 		return result;
 	}
 	
+		//Vincent Lauren
+	public static HashMap<String, Double> getAverageNumberOfPublicationsInAYear(String fileName)
+	{
+		HashMap<String, Double> result = new HashMap<String, Double>();
+		List<Integer> publicationForYear = new LinkedList<Integer>();
+		
+		UserStory3 us3 = new UserStory3();
+		
+		List<String> listYear = us3.get_list_of_years_appearancesC(fileName);
+		
+		for(int i = 0;i<listYear.size();i++)
+		{
+			publicationForYear.add(us3.get_number_of_publications_per_year(fileName,listYear.get(i)));
+		}
+		
+		result.put("mean", Calcul.mean(publicationForYear));
+		result.put("median", Calcul.median(publicationForYear));
+		result.put("mode", Calcul.mode(publicationForYear));
+		
+		return result;
+	}
+	
+	public static HashMap<String, Double> getAverageNumberOfpublicationsConferenceInAYear(String fileName)
+	{
+		HashMap<String, Double> result = new HashMap<String, Double>();
+		List<Integer> confpublicationForYear = new LinkedList<Integer>();
+		
+		UserStory3 us3 = new UserStory3();
+		
+		List<String> listYear = us3.get_list_of_years_appearancesC(fileName);
+		
+		for(int i = 0;i<listYear.size();i++)
+		{
+			confpublicationForYear.add(us3.get_number_of_publicationsConference_per_year(fileName,listYear.get(i)));
+		}
+		
+		result.put("mean", Calcul.mean(confpublicationForYear));
+		result.put("median", Calcul.median(confpublicationForYear));
+		result.put("mode", Calcul.mode(confpublicationForYear));
+		
+		return result;
+	}
+	
+	public static HashMap<String, Double> getAverageNumberOfJournalArticlesInAYear(String fileName)
+	{
+		HashMap<String, Double> result = new HashMap<String, Double>();
+		List<Integer> journalArticleForYear = new LinkedList<Integer>();
+		
+		UserStory3 us3 = new UserStory3();
+		
+		List<String> listYear = us3.get_list_of_years_appearancesC(fileName);
+		
+		for(int i = 0;i<listYear.size();i++)
+		{
+			journalArticleForYear.add(us3.get_number_of_journalArticles_per_year(fileName,listYear.get(i)));
+		}
+		
+		result.put("mean", Calcul.mean(journalArticleForYear));
+		result.put("median", Calcul.median(journalArticleForYear));
+		result.put("mode", Calcul.mode(journalArticleForYear));
+		
+		return result;
+	}
+
+	public static HashMap<String, Double> getAverageNumberOfBookInAYear(String fileName)
+	{
+		HashMap<String, Double> result = new HashMap<String, Double>();
+		List<Integer> bookForYear = new LinkedList<Integer>();
+		
+		UserStory3 us3 = new UserStory3();
+		
+		List<String> listYear = us3.get_list_of_years_appearancesC(fileName);
+		
+		for(int i = 0;i<listYear.size();i++)
+		{
+			bookForYear.add(us3.get_number_of_book_per_year(fileName,listYear.get(i)));
+		}
+		
+		result.put("mean", Calcul.mean(bookForYear));
+		result.put("median", Calcul.median(bookForYear));
+		result.put("mode", Calcul.mode(bookForYear));
+		
+		return result;
+	}
+	
+
+	public static HashMap<String, Double> getAverageNumberOfBookChapterInAYear(String fileName)
+	{
+		HashMap<String, Double> result = new HashMap<String, Double>();
+		List<Integer> chapterbookForYear = new LinkedList<Integer>();
+		
+		UserStory3 us3 = new UserStory3();
+		
+		List<String> listYear = us3.get_list_of_years_appearancesC(fileName);
+		
+		for(int i = 0;i<listYear.size();i++)
+		{
+			chapterbookForYear.add(us3.get_number_of_book_per_year(fileName,listYear.get(i)));
+		}
+		
+		result.put("mean", Calcul.mean(chapterbookForYear));
+		result.put("median", Calcul.median(chapterbookForYear));
+		result.put("mode", Calcul.mode(chapterbookForYear));
+		
+		return result;
+	}
+	
 }
--- test/fr/dauphine/publications_analytics/test/YearTest.java
+++ test/fr/dauphine/publications_analytics/test/YearTest.java
@@ -2,6 +2,8 @@ package fr.dauphine.publications_analytics.test;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+
 import org.junit.Test;
 
 import fr.dauphine.publications_analytics_task3.src.Year;
@@ -11,9 +13,13 @@ public class YearTest {
 	@Test
 	public void TestPrincipal()
 	{
-		String fileName = "dblp_curated_sample.xml";
-		//String fileName = "dblp_2.xml";
-		
+		//String fileName = "dblp_curated_sample.xml";
+		String fileName = "dblp_2.xml";
+		Question3ATest(fileName);
+		Question3BTest(fileName);
+		Question3CTest(fileName);
+		Question3DTest(fileName);
+		Question3ETest(fileName);
 		Question4ATest(fileName);
 		Question4BTest(fileName);
 		Question4CTest(fileName);
@@ -109,6 +115,101 @@ public class YearTest {
 		double meanResult = Year.GetAverageNumberAuthorForBookChaptersInAYear(fileName).get("mean");
 		double medianResult = Year.GetAverageNumberAuthorForBookChaptersInAYear(fileName).get("median");
 		double modeResult = Year.GetAverageNumberAuthorForBookChaptersInAYear(fileName).get("mode");
+		
+		//Assert
+		assertEquals(meanTest, meanResult, 0);
+		assertEquals(medianTest, medianResult, 0);
+		assertEquals(modeTest, modeResult, 0);
+	}
+	
+	//3.A
+	public void Question3ATest(String fileName) {
+
+		//Arrange
+		double meanTest = 1.25;
+		double medianTest = 1.0;
+		double modeTest = 1.0;
+		
+		//Act
+		double meanResult = Year.getAverageNumberOfPublicationsInAYear(fileName).get("mean");
+		double medianResult = Year.getAverageNumberOfPublicationsInAYear(fileName).get("median");
+		double modeResult = Year.getAverageNumberOfPublicationsInAYear(fileName).get("mode");
+		
+		//Assert
+		assertEquals(meanTest, meanResult, 0);
+		assertEquals(medianTest, medianResult, 0);
+		assertEquals(modeTest, modeResult, 0);
+	}
+	
+	//3.B
+	public void Question3BTest(String fileName) {
+
+		//Arrange
+		double meanTest = 0.25;
+		double medianTest = 0.0;
+		double modeTest = 0.0;
+		
+		//Act
+		double meanResult = Year.getAverageNumberOfpublicationsConferenceInAYear(fileName).get("mean");
+		double medianResult = Year.getAverageNumberOfpublicationsConferenceInAYear(fileName).get("median");
+		double modeResult = Year.getAverageNumberOfpublicationsConferenceInAYear(fileName).get("mode");
+		
+		//Assert
+		assertEquals(meanTest, meanResult, 0);
+		assertEquals(medianTest, medianResult, 0);
+		assertEquals(modeTest, modeResult, 0);
+	}
+
+	//3.C
+	public void Question3CTest(String fileName) {
+
+		//Arrange
+		double meanTest = 0.5;
+		double medianTest = 1.0;
+		double modeTest = 0.0;
+		
+		//Act
+		double meanResult = Year.getAverageNumberOfJournalArticlesInAYear(fileName).get("mean");
+		double medianResult = Year.getAverageNumberOfJournalArticlesInAYear(fileName).get("median");
+		double modeResult = Year.getAverageNumberOfJournalArticlesInAYear(fileName).get("mode");
+		
+		//Assert
+		assertEquals(meanTest, meanResult, 0);
+		assertEquals(medianTest, medianResult, 0);
+		assertEquals(modeTest, modeResult, 0);
+	}
+	
+	//3.D
+	public void Question3DTest(String fileName) {
+
+		//Arrange
+		double meanTest = 0.25;
+		double medianTest = 0.0;
+		double modeTest = 0.0;
+		
+		//Act
+		double meanResult = Year.getAverageNumberOfBookInAYear(fileName).get("mean");
+		double medianResult = Year.getAverageNumberOfBookInAYear(fileName).get("median");
+		double modeResult = Year.getAverageNumberOfBookInAYear(fileName).get("mode");
+		
+		//Assert
+		assertEquals(meanTest, meanResult, 0);
+		assertEquals(medianTest, medianResult, 0);
+		assertEquals(modeTest, modeResult, 0);
+	}
+	
+	//3.E
+	public void Question3ETest(String fileName) {
+
+		//Arrange
+		double meanTest = 0.25;
+		double medianTest = 0.0;
+		double modeTest = 0.0;
+		
+		//Act
+		double meanResult = Year.getAverageNumberOfBookChapterInAYear(fileName).get("mean");
+		double medianResult = Year.getAverageNumberOfBookChapterInAYear(fileName).get("median");
+		double modeResult = Year.getAverageNumberOfBookChapterInAYear(fileName).get("mode");
 		
 		//Assert
 		assertEquals(meanTest, meanResult, 0);
