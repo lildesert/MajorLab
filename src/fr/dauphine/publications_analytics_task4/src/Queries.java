f1f7e5da859cfcea45312b27dfd67c36f6d5208e
duda12 <dudavidgm@gmail.com>
2014-05-27 15:09:45 +0200

    test task4 question 1

--- src/fr/dauphine/publications_analytics_task4/src/Queries.java
+++ src/fr/dauphine/publications_analytics_task4/src/Queries.java
@@ -1,5 +1,53 @@
 package fr.dauphine.publications_analytics_task4.src;
 
+import java.util.HashMap;
+
+import javax.xml.xquery.XQConnection;
+import javax.xml.xquery.XQDataSource;
+import javax.xml.xquery.XQException;
+import javax.xml.xquery.XQExpression;
+import javax.xml.xquery.XQSequence;
+
+import net.sf.saxon.xqj.SaxonXQDataSource;
+
 public class Queries {
+	//Question 1
+	public HashMap<String, Integer> getNumberOfAuthorAppearances(String file_name,String authorName) {
+
+		String xml_file = getClass().getResource(file_name).toExternalForm();
+		
+		String query = "for $x in doc(\"" + xml_file + "\")/dblp where $x/author = " + authorName
+				+ " return distinct-values(for $y in $x/author where $y/author != " + authorName 
+				+" return $y/author/text())";
+		
+		System.out.println("XQuery query:" + query);
+		HashMap<String, Integer> listAuthor = new HashMap();
+
+		try {
+			XQDataSource ds = new SaxonXQDataSource();
+			XQConnection conn = ds.getConnection();
+			XQExpression exp = conn.createExpression();
+			XQSequence seq = exp.executeQuery(query);
+			
+			XQExpression exp2 = conn.createExpression();
+			XQSequence seq2;
+			
+			while(seq.next())	{
+			
+				String query2 = "for $x in doc(\"" + xml_file + "\")/dblp where $x/author = " + seq.getAtomicValue()
+						+ " return distinct-values(for $y in $x/author "
+						+ " return $y/author/text())";
+				seq2 = exp.executeQuery(query2);
+				listAuthor.put(seq.getAtomicValue(), seq2.count());
+			}
+			seq.close();
+			
+		} catch (XQException err) {
+			System.out.println("Failed as expected: " + err.getMessage());
+		}
+
+		return listAuthor;
+
+	}
 
 }
---
+++ test/fr/dauphine/publications_analytics/test/CoothorsTest.java
@@ -0,0 +1,30 @@
+package fr.dauphine.publications_analytics.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import fr.dauphine.publications_analytics_task4.src.Queries;
+
+public class CoothorsTest {
+	@Test
+	public void TestPrincipal() throws Exception
+	{
+		String file_name = "dblp_2.xml";
+		Question1Test(file_name);
+	}
+	
+	public void Question1Test(String file_name) throws Exception	{
+		HashMap<String, Integer> result = new HashMap();
+		Queries queries = new Queries ();
+		String author = "Stefano Ceri";
+		result = queries.getNumberOfAuthorAppearances(file_name, author);
+		System.out.println(result.get("Piero Fraternali"));
+		assertEquals(1, result.get("Piero Fraternali"),0);
+
+		
+	}
+
+}
