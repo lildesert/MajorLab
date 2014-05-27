c802295d9f615a3a3a2cc195e21aad4c92cba267
JBISTOQU <JBISTOQU@BTW71244.bt0d0000.w2k.bouyguestelecom.fr>
2014-05-27 16:30:09 +0200

    Ajouts UI

--- src/UI/FormRequest2.java
+++ src/UI/FormRequest2.java
@@ -1,56 +1,31 @@
 package UI;
 
-import java.awt.EventQueue;
-import java.awt.Window;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 import fr.dauphine.publications_analytics.src.UserStory2;
 
-import java.awt.Font;
-import java.util.List;
-
-import javax.swing.JScrollPane;
-
-public class FormRequest1 extends JFrame {
+public class FormRequest2 extends JPanel {
 
 	private JPanel contentPane;
-
 	/**
-	 * Launch the application.
+	 * Create the panel.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FormRequest1 frame = new FormRequest1();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	public FormRequest2() {
 
-	/**
-	 * Create the frame.
-	 */
-	public FormRequest1() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JButton btnRetour = new JButton("Retour");
@@ -87,4 +62,5 @@ public class FormRequest1 extends JFrame {
 		scrollPane.setViewportView(lbAuthor);
 		lbAuthor.setValueIsAdjusting(true);
 	}
+
 }
--- src/UI/Home.java
+++ src/UI/Home.java
@@ -1,23 +1,31 @@
 package UI;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Container;
 import java.awt.EventQueue;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
-
-import java.awt.BorderLayout;
-
-import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.Font;
 
-public class Home {
+public class Home implements ItemListener {
 
 	private JFrame frame;
+	private JPanel cards;
+	
+	final static String BUTTONPANEL = "Card with JButtons";
+    final static String TEXTPANEL = "Card with JTextField";
 
 	/**
 	 * Launch the application.
@@ -26,14 +34,61 @@ public class Home {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Home window = new Home();
-					window.frame.setVisible(true);
+					createAndShowGUI();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+	
+	public void itemStateChanged(ItemEvent evt) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
+    }
+	
+	/**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("CardLayoutDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         
+        //Create and set up the content pane.
+        Home window = new Home();
+        window.addComponentToPane(frame.getContentPane());
+         
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public void addComponentToPane(Container pane) {
+        //Put the JComboBox in a JPanel to get a nicer look.
+        JPanel comboBoxPane = new JPanel(); //use FlowLayout
+        String comboBoxItems[] = { BUTTONPANEL, TEXTPANEL };
+        JComboBox cb = new JComboBox(comboBoxItems);
+        cb.setEditable(false);
+        cb.addItemListener(this);
+        comboBoxPane.add(cb);
+         
+        //Create the "cards".
+        FormRequest2 card1 = new FormRequest2();
+        
+        JPanel card2 = new JPanel();
+        card2.add(new JTextField("TextField", 20));
+         
+        //Create the panel that contains the "cards".
+        cards = new JPanel(new CardLayout());
+        cards.add(card1, BUTTONPANEL);
+        cards.add(card2, TEXTPANEL);
+         
+        pane.add(comboBoxPane, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
+    }
 
 	/**
 	 * Create the application.
@@ -46,7 +101,8 @@ public class Home {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
+		/*frame = new JFrame();
+		frame.setVisible(true);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -55,7 +111,6 @@ public class Home {
 		btnForm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				FormRequest1 f = new FormRequest1();
-				frame.setVisible(false);
 				f.setVisible(true);
 			}
 		});
@@ -65,7 +120,7 @@ public class Home {
 		JLabel lblAuthorCollaborations = new JLabel("Author Collaborations");
 		lblAuthorCollaborations.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblAuthorCollaborations.setBounds(134, 11, 172, 44);
-		frame.getContentPane().add(lblAuthorCollaborations);
+		frame.getContentPane().add(lblAuthorCollaborations);*/
 	}
 	
 	public static void infoBox(String infoMessage, String location)
