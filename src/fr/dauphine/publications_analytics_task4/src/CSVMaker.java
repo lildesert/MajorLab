40bd965f0ec6e287142d95c35fceae19ed2e3b5a
djeffou <jeffrey.lucas23@gmail.com>
2014-06-03 14:27:37 +0200

    Fonction statique permettant de créer un fichier excel à partir d'une hashmap

---
+++ src/fr/dauphine/publications_analytics_task4/src/CSVMaker.java
@@ -0,0 +1,32 @@
+package fr.dauphine.publications_analytics_task4.src;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.HashMap;
+
+public class CSVMaker {
+	
+	public static void makeCSV(String filename, HashMap<String, Integer> listAuthors) {
+		File outputFile = new File(filename); 
+		try {
+			FileWriter out = new FileWriter(outputFile);
+			
+			for (Entry<String, Integer> e : listAuthors.entrySet()) {
+			    String key = e.getKey();
+			    Object value = e.getValue();
+			    
+			    out.write(key);
+			    out.write(',');
+			    out.write(value.toString());
+			    out.write('\n');
+			    
+			    out.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
